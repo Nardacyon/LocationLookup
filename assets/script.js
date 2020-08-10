e5d615bb664213d92c6e6609e526bbb0891efa82
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-08-10 10:37:27 -0700

    added defer attributes and Urls that will be changed later

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,23 @@
-mapsKey = "AIzaSyCV28S3i7AYMjrj5nMX99lyOo6zp8w6Gz4";
-
-mapsURL = "https://maps.googleapis.com/maps/api/js?key=" + mapsKey + "&callback=initMap"
+const mapsKey = "KAWAMdLGHWecahgkpIBMIHCAvzGhCgeG";
+var locationInput = "San%20Diego,CA";
+const mapsURL = "http://www.mapquestapi.com/geocoding/v1/address?key=" + mapsKey + "&location=" + locationInput;
 
 $.ajax({
     url: mapsURL,
-    method: "GET"
+    method: "GET",
 })
-
 .then(function(response) {
     console.log(response)
+    console.log(response.results[0].locations[0].mapUrl)
 });
 
-console.log(mapsURL)
+const censusKey = "a0c1493a1c79e4680d709f96001b49d5df683b82";
+const censusUrl = "https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&DATE_=7&for=state:*&key=" + censusKey;
+
+$.ajax({
+    url: censusUrl,
+    method: "GET",
+})
+.then(function(response) {
+    console.log(response)
+})
