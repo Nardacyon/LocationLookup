4bf8a399d42f02f9603e37bbad12256a7b8b9a72
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-08-13 11:39:22 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,26 @@
-// const mapsKey = "KAWAMdLGHWecahgkpIBMIHCAvzGhCgeG";
-// var locationInput = "San%20Diego,CA";
-// const mapsURL = "http://www.mapquestapi.com/geocoding/v1/address?key=" + mapsKey + "&location=" + locationInput;
+const mapsKey = "KAWAMdLGHWecahgkpIBMIHCAvzGhCgeG";
+var locationInput = "San%20Diego,CA";
+const mapsURL = "http://www.mapquestapi.com/geocoding/v1/address?key=" + mapsKey + "&location=" + locationInput;
 
-// $.ajax({
-//     url: mapsURL,
-//     method: "GET",
-// })
-// .then(function(response) {
-//     console.log(response)
-//     console.log(response.results[0].locations[0].mapUrl)
-// });
+$.ajax({
+    url: mapsURL,
+    method: "GET",
+})
+.then(function(response) {
+    console.log(response)
+    console.log(response.results[0].locations[0].mapUrl)
+});
 
-// const censusKey = "a0c1493a1c79e4680d709f96001b49d5df683b82";
-// const censusUrl = "https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&DATE_=7&for=state:*&key=" + censusKey;
+const censusKey = "a0c1493a1c79e4680d709f96001b49d5df683b82";
+const censusUrl = "https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&DATE_=7&for=state:*&key=" + censusKey;
 
-// $.ajax({
-//     url: censusUrl,
-//     method: "GET",
-// })
-// .then(function(response) {
-//     // console.log(response)
-// })
+$.ajax({
+    url: censusUrl,
+    method: "GET",
+})
+.then(function(response) {
+    // console.log(response)
+})
 
 let weatherAPI = "4392f23b16ef4173136c90ec556dff94";
 
