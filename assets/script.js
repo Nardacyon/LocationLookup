a303e9a04265e66cf94c71c3f3a7e39421249bc5
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-08-14 12:28:04 -0700

    created map and weather cards

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,38 @@
-const mapsKey = "KAWAMdLGHWecahgkpIBMIHCAvzGhCgeG";
-var locationInput = "San%20Diego,CA";
-const mapsURL = "http://www.mapquestapi.com/geocoding/v1/address?key=" + mapsKey + "&location=" + locationInput;
+// const mapsKey = "pk.eyJ1IjoibmFyZGFjeW9uIiwiYSI6ImNrZHVpYnYydzA4NHQyeW9namNzYW5ncDQifQ.d9XwB7yRUuZAtnXx-4pGHQ";
+// var locationInput = "San%20Diego,CA";
 
-$.ajax({
-    url: mapsURL,
-    method: "GET",
-})
-.then(function(response) {
-    console.log(response)
-    console.log(response.results[0].locations[0].mapUrl)
-});
+// const corsAnywherePrefix = 'https://cors-anywhere.herokuapp.com/';
+// const mapsURL = "https://api.mapbox.com/?access_token=" + mapsKey;
 
-const censusKey = "a0c1493a1c79e4680d709f96001b49d5df683b82";
-const censusUrl = "https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&DATE_=7&for=state:*&key=" + censusKey;
+// $.ajax({
+//     url: corsAnywherePrefix + mapsURL,
+//     method: 'GET'
+// })
+// .then(function (responseMaps){
+//     console.log(responseMaps);
+//     var map = new mapboxgl.Map({
+//         container: 'map',
+//         style: 'mapbox://styles/mapbox/light-v10',
+//         center: [-96, 37.8],
+//         zoom: 3
+//       });
+// })
+// .catch(function (error) {
+//     console.log(error);
+// });
 
-$.ajax({
-    url: censusUrl,
-    method: "GET",
-})
-.then(function(response) {
-    console.log(response)
-})
-
-const mapsKey = "KAWAMdLGHWecahgkpIBMIHCAvzGhCgeG";
-var locationInput = "San%20Diego,CA";
-const mapsURL = "http://www.mapquestapi.com/geocoding/v1/address?key=" + mapsKey + "&location=" + locationInput;
-
-$.ajax({
-    url: mapsURL,
-    method: "GET",
-})
-.then(function(response) {
-    console.log(response)
-    console.log(response.results[0].locations[0].mapUrl)
-});
+// var map = new ol.Map({
+//     target: 'map',
+//     layers: [
+//       new ol.layer.Tile({
+//         source: new ol.source.OSM()
+//       })
+//     ],
+//     view: new ol.View({
+//       center: ol.proj.fromLonLat([-117.16, 32.72]),
+//       zoom: 4
+//     })
+//   });
 
 const censusKey = "a0c1493a1c79e4680d709f96001b49d5df683b82";
 const censusUrl = "https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&DATE_=7&for=state:*&key=" + censusKey;
@@ -49,7 +48,22 @@ $.ajax({
 let weatherAPI = "4392f23b16ef4173136c90ec556dff94";
 
 $(document).ready(function () {
-     
+    //coord for SD map will generally show SoCal
+    let longitude = -117.16;
+    let latitude = 32.72;
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([longitude, latitude]),
+          zoom: 4
+        })
+      });
+
     $(".submit").on("click", function(event) {
         event.preventDefault();
         let searchLocation = $("#search-input").val();
@@ -65,7 +79,6 @@ $(document).ready(function () {
     })
     .then(function (responseWeather) {
         console.log(responseWeather)
-
         let weatherIconCode = responseWeather.weather[0].icon;
         var weatherIconLink = "http://openweathermap.org/img/wn/" + weatherIconCode + ".png"
 
@@ -78,8 +91,43 @@ $(document).ready(function () {
         weatherCard.append(cardHeader, weatherIcon, temp, humidity)
         $(".forecast").append(weatherCard)
 
-        
-        // let weatherIconCode = responseWeather.
+        const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchLocation + "&appid=" + weatherAPI + "&units=imperial";
+
+        for(let i = 0; i < 5; i++) {    
+
+            let selectArr = 2 + (i * 8);
+            
+            $.ajax({
+                url: forecastUrl,
+                method: "GET"
+            })
+            .then(function(responseForecast) {
+
+                let forecastIconCode = responseForecast.list[selectArr].weather[0].icon;
+                var forecastIconLink = "http://openweathermap.org/img/wn/" + forecastIconCode + ".png"
+
+                let forecastCard = $("<div>").addClass("forecast-card");
+
+                let date = (moment.unix(responseForecast.list[selectArr].dt).format("MM/DD/YYYY"));
+                let forecastDate = $("<h5>").text(date);
+                forecastDate.addClass("forecast-date");
+
+                let forecastIcon = $("<img>").attr("src", forecastIconLink);
+
+                let forecastTemp = $("<p>").text("Temp: " + responseForecast.list[selectArr].main.temp + "\xB0F");
+                forecastTemp.addClass("card-text");
+
+                let forecastHumidity = $("<p>").text("Humidity: " + responseForecast.list[selectArr].main.humidity + "%");
+                forecastHumidity.addClass("card-text");
+
+                forecastCard.append(forecastDate);
+                forecastCard.append(forecastIcon);
+                forecastCard.append(forecastTemp);
+                forecastCard.append(forecastHumidity);
+                
+                $(".forecast").append(forecastCard);
+            })
+        }
     });
 
     }
